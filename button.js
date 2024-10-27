e54b5429ed8b1166c6def03ada26f3af3ed19958
MandalaGayathri-01 <mandalagayatri01@gmail.com>
2024-10-27 20:14:59 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,3 @@
+document.getElementById("alertButton").addEventListener("click", function() {
+    alert("Fun Fact: Vijayawada is known for its traditional 'Pootharekulu', a sweet delicacy made with rice flour and jaggery!");
+});
